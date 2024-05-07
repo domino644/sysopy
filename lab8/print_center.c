8e1936c266f022ab4e150b629824a994f255c5ed
domino644 <cichemleko@gmail.com>
2024-05-07 14:32:36 +0200

    lab8

--- lab8/print_center.c
+++ lab8/print_center.c
@@ -16,7 +16,6 @@ struct shared_memory {
     char print_queue[MAX_QUEUE_SIZE][TEXT_SIZE];
     int queue_front;
     int queue_rear;
-    int num_printers_available;
 };
 
 void wait_semaphore(int sem_id, int sem_num) {
@@ -42,13 +41,13 @@ int main() {
     union semun sem_union;
     int sem_id = semget(key, 3, IPC_CREAT | 0666);
 
-    sem_union.val = 1; // Semaphore for print queue access
+    sem_union.val = 1;
     semctl(sem_id, 0, SETVAL, sem_union);
 
-    sem_union.val = MAX_QUEUE_SIZE; // Semaphore for queue space available
+    sem_union.val = MAX_QUEUE_SIZE;
     semctl(sem_id, 1, SETVAL, sem_union);
 
-    sem_union.val = 1; // Semaphore for printer availability
+    sem_union.val = 1;
     semctl(sem_id, 2, SETVAL, sem_union);
 
     srand(time(NULL));
@@ -61,19 +60,18 @@ int main() {
                     text[j] = 'a' + rand() % 26;
                 }
 
-                wait_semaphore(sem_id, 1); // Wait for space in queue
-                wait_semaphore(sem_id, 0); // Wait for access to queue
+                wait_semaphore(sem_id, 1);
+                wait_semaphore(sem_id, 0);
 
                 if ((shared_mem->queue_rear + 1) % MAX_QUEUE_SIZE != shared_mem->queue_front) {
-                    // There is space in the queue
                     strcpy(shared_mem->print_queue[shared_mem->queue_rear], text);
                     shared_mem->queue_rear = (shared_mem->queue_rear + 1) % MAX_QUEUE_SIZE;
                 }
 
-                signal_semaphore(sem_id, 0); // Release access to queue
-                signal_semaphore(sem_id, 2); // Signal printer availability
+                signal_semaphore(sem_id, 0);
+                signal_semaphore(sem_id, 2);
 
-                sleep(rand() % 5 + 1); // Wait for random time
+                sleep(rand() % 5 + 1);
             }
         }
     }
@@ -81,30 +79,35 @@ int main() {
     for (int i = 0; i < MAX_PRINTERS; i++) {
         if (fork() == 0) {
             while (1) {
-                wait_semaphore(sem_id, 2); // Wait for printer availability
-                wait_semaphore(sem_id, 0); // Wait for access to queue
+                wait_semaphore(sem_id, 2);
+                wait_semaphore(sem_id, 0);
 
                 if (shared_mem->queue_front != shared_mem->queue_rear) {
-                    // There are pending print jobs in the queue
-                    printf("Printing: %s\n", shared_mem->print_queue[shared_mem->queue_front]);
+                    int i = 0;
+                    while (i < TEXT_SIZE) {
+                        if (shared_mem->print_queue[shared_mem->queue_front][i]) {
+                            printf("%c\n", shared_mem->print_queue[shared_mem->queue_front][i]);
+                        }
+                        i++;
+                        sleep(1);
+                    }
                     shared_mem->queue_front = (shared_mem->queue_front + 1) % MAX_QUEUE_SIZE;
-                    sleep(1);
                 }
 
-                signal_semaphore(sem_id, 0); // Release access to queue
-                signal_semaphore(sem_id, 1); // Signal queue space available
+                signal_semaphore(sem_id, 0);
+                signal_semaphore(sem_id, 1);
 
                 sleep(1);
             }
         }
     }
 
-    // Wait for children to finish
+
     for (int i = 0; i < MAX_USERS + MAX_PRINTERS; i++) {
         wait(NULL);
     }
 
-    // Clean up
+
     shmdt(shared_mem);
     shmctl(shmid, IPC_RMID, NULL);
     semctl(sem_id, 0, IPC_RMID, sem_union);
